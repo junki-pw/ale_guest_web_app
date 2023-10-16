ce68602093a0970506256560919f23bcffbaa34c
junki-pw <juken.master1234@gmail.com>
2023-10-16 16:06:23 +0900

    category tile 追加

---
+++ src/app/order-rooms/[orderRoomId]/menus/components/category_tile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CategoryTile = () => {
+    return (
+        <h1 className="mx-4 mb-3 mt-4 font-bold line-clamp-1 text-xl">
+            カテゴリー名カテゴリー名カテゴリー名カテゴリー名カテゴリー名カテゴリー名
+        </h1>
+    );
+};
+
+export default CategoryTile;
