5b54efa5f3ea7073f15ab73692751868cc9b0311
junki-pw <juken.master1234@gmail.com>
2023-10-16 16:08:33 +0900

    key 追加

--- src/app/order-rooms/[orderRoomId]/menus/page.tsx
+++ src/app/order-rooms/[orderRoomId]/menus/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { MenuTile } from './components/menu_tile'
 import CategoryTile from './components/category_tile'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function MenusPage() {
     const arr = ["リンゴ", "パイナップル", "ペン"];
@@ -15,8 +16,12 @@ export default function MenusPage() {
         <main>
             {list.map((fruit, i) =>
                 <div>
-                    <CategoryTile />
-                    {arr.map((e) => <MenuTile />)}
+                    <CategoryTile
+                        key={uuidv4()}
+                    />
+                    {arr.map((e) => <MenuTile
+                        key={uuidv4()}
+                    />)}
                 </div>
             )}
         </main>
