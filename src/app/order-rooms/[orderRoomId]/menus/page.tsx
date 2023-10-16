f7a4cfd159d82ca6383cece9907d980fe9314432
junki-pw <juken.master1234@gmail.com>
2023-10-16 16:34:32 +0900

    MenusBottomButton 追加

--- src/app/order-rooms/[orderRoomId]/menus/page.tsx
+++ src/app/order-rooms/[orderRoomId]/menus/page.tsx
@@ -3,16 +3,17 @@
 import { MenuTile } from './components/menu_tile'
 import CategoryTile from './components/category_tile'
 import { v4 as uuidv4 } from 'uuid';
+import MenusBottomButton from './components/menus_bottom_button';
 
 export default function MenusPage() {
     const arr = ["リンゴ", "パイナップル", "ペン"];
 
     const list = [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1,
     ];
 
     return (
-        <main>
+        <main className='relative pb-40'>
             {list.map((fruit) =>
                 <div key={uuidv4()}>
                     {/* カテゴリー */}
@@ -22,6 +23,8 @@ export default function MenusPage() {
                     {arr.map((e) => <MenuTile key={uuidv4()} />)}
                 </div>
             )}
+
+            <MenusBottomButton />
         </main>
     )
 }
