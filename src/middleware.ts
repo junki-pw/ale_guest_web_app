061eee6668791a284be211fc5bc2b322a7a889c8
junki-pw <juken.master1234@gmail.com>
2023-10-18 20:11:32 +0900

    middleware ファイル位置の修正

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { auth } from "@/providers/firebase";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const url = req.url;
+  const parsedUrl = new URL(url);
+  const pathName = parsedUrl.pathname;
+  console.log("RedirectURL: " + pathName);
+  const firebaseUser = auth.currentUser;
+  console.log("firebaseUser: " + firebaseUser?.uid);
+
+  //todo 認証していない場合は認証ページに飛ばす
+
+  // return NextResponse.redirect(new URL("/redirect", req.url));
+}
+
+export const config = {
+  matcher: ["/", "/order-rooms"],
+};
