4615d75156928e2b3965e151b75720240c29c2b3
junki-pw <juken.master1234@gmail.com>
2023-10-19 15:31:09 +0900

    認証実験の修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,63 @@
 "use client";
 import { auth } from "@/providers/firebase";
-import { signInAnonymously } from "firebase/auth";
+import { setUserId } from "firebase/analytics";
+import {
+  browserSessionPersistence,
+  onAuthStateChanged,
+  setPersistence,
+  signInAnonymously,
+} from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+// Sleep関数
+// export function sleep(ms: number) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
 export default function LoginPage() {
-  const [uid, setUid] = useState<string | null>(null);
+  const [uid, setUid] = useState<string | null | undefined>(null);
   const router = useRouter();
   const handleSignIn = async () => {
     try {
-      await signInAnonymously(auth).then((value) => {
-        console.log("匿名認証成功: " + value.user?.uid);
-        setUid(value.user.uid);
-      });
+      setPersistence(auth, browserSessionPersistence)
+        .then(() => {
+          // Existing and future Auth states are now persisted in the current
+          // session only. Closing the window would clear any existing state even
+          // if a user forgets to sign out.
+          // ...
+          // New sign-in will be persisted with session persistence.
+          signInAnonymously(auth).then((value) => {
+            console.log("匿名認証成功: " + value.user?.uid);
+            setUid(value.user.uid);
+          });
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
     } catch (e) {
       alert("エラーが発生しました");
     } finally {
     }
+  };
+
+  //   useEffect(() => {
+  //     setUid(auth.currentUser?.uid);
+  //     delay();
+  //   }, []);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      setUid(user?.uid);
+    });
+  }, []);
+
+  const delay = async () => {
+    // await sleep(3000);
+    setUid(auth.currentUser?.uid);
+    console.log("ログインページ2 uid: " + auth.currentUser?.uid);
   };
 
   console.log("ログインページ uid: " + auth.currentUser?.uid);
@@ -37,6 +78,11 @@ export default function LoginPage() {
         >
           匿名認証ログイン
         </button>
+
+        <div className="mb-4"></div>
+        <button className="mt-4" onClick={delay}>
+          データを取得する
+        </button>
       </main>
     );
   }
@@ -44,6 +90,13 @@ export default function LoginPage() {
   return (
     <main>
       <h1>firebaseUserId: {uid}</h1>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        画面遷移する
+      </button>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <h1>HPのホーム画面 今後はHPでありアプリであるようにする</h1>;
+  const router = useRouter();
+  return (
+    <main>
+      <h1>HPのホーム画面 今後はHPでありアプリであるようにする</h1>
+      <button
+        onClick={() => {
+          router.push("/login");
+        }}
+      >
+        ログインページに画面遷移
+      </button>
+    </main>
+  );
 }
