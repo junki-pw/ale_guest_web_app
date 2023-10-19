fa7edb3c78bf269978167d661be0155ae0799e11
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-10-19 20:32:24 +0900

    写真に全て変更

--- src/app/order-rooms/[orderRoomId]/check-join/page.tsx
+++ src/app/order-rooms/[orderRoomId]/check-join/page.tsx
@@ -5,28 +5,14 @@ import { faXmark, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function Page() {
   return (
     <main className="">
-      <header className="bg-white p-10 border-b-2 border-gray-200">
-        <nav className="flex justify-between ">
-          <div className="font-bold">
-            <a
-              href=""
-              className="text-red-500 hover:text-green-400 transition-all duration-300 md:text-3xl"
-            >
-              <FontAwesomeIcon
-                icon={faXmark}
-                style={{ color: "gray" }}
-                className="w-5 h-5 m-auto"
-              />
-            </a>
-          </div>
-          <div className="mx-auto text-2xl font-bold text-gray-800">
-            グループの参加確認
-          </div>
-        </nav>
-      </header>
-
       <nav className="flex flex-col justify-center items-center ">
-        <Image src={"https://placehold.jp/150x150.png"} alt={"shop_icon_url"} width={72} height={72} className="my-2"></Image>
+        <Image
+          src={"https://placehold.jp/150x150.png"}
+          alt={"shop_icon_url"}
+          width={72}
+          height={72}
+          className="my-6 rounded-full"
+        ></Image>
         <div className="font-bold text-2xl text-gray-800 mb-2">
           ガンジャバンギラス
         </div>
@@ -38,7 +24,13 @@ export default function Page() {
         <div className="py-4 font-bold text-gray-600 text-xl">お店について</div>
         <div className="flex justify-between">
           <div className="flex ">
-            <div className=" bg-gray-600 rounded-full h-20 w-20 flex justify-center"></div>
+            <Image
+              src={"https://placehold.jp/150x150.png"}
+              alt={"shop_icon_url"}
+              width={32}
+              height={32}
+              className="my-6 rounded-full"
+            ></Image>
             <div className="px-4 py-4">
               <div className=" font-bold text-gray-800">
                 店舗名店舗名店舗名店舗名店舗名
@@ -61,30 +53,62 @@ export default function Page() {
           お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明お席の説明
         </div>
         <div className="py-4 flex overflow-x-auto">
-          <div
-            className="mr-2 bg-gray-600  rounded
-           h-40 w-40 flex justify-center flex-none"
-          ></div>
-          <div
-            className="mr-2 bg-gray-600  rounded
-           h-40 w-40 flex justify-center flex-none"
-          ></div>{" "}
-          <div
-            className="mr-2 bg-gray-600  rounded
-         h-40 w-40 flex justify-center flex-none"
-          ></div>{" "}
-          <div
-            className="mr-2 bg-gray-600  rounded
-       h-40 w-40 flex justify-center flex-none"
-          ></div>{" "}
-          <div
-            className="mr-2 bg-gray-600  rounded
-     h-40 w-40 flex justify-center flex-none"
-          ></div>{" "}
-          <div
-            className="mr-2 bg-gray-600  rounded
-   h-40 w-40 flex justify-center flex-none"
-          ></div>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={64}
+            height={64}
+            className="my-6 rounded-lg mx-1"
+          ></Image>
         </div>
         <div className="pb-2 pt-8  font-bold text-gray-600 text-xl">
           突き出し・テーブルチャージ
@@ -98,63 +122,74 @@ export default function Page() {
           <div>￥5000/席</div>
         </div>
         <div className="pb-2 pt-8  font-bold text-gray-600 text-xl">
-          「ルームなう」に共有
-        </div>
-        <div className="text-gray-400">
-          どのお店でお食事をしているかをフォロワーに共有できます。
-          <br />
-          今どこにいるか分かる場合があるため注意してください。
-        </div>
-        <div className="py-5">
-          <input
-            type="checkbox"
-            id=""
-            name=""
-            className=" focus:bg-green-600 hover:text-green-500 w-6 h-6"
-          />
-          <label htmlFor="" className="font-bold px-2 text-xl text-gray-6ﬁ00 ">
-            「ルームなう」に共有を許可する
-          </label>
-        </div>
-        <div className="pb-2 pt-8  font-bold text-gray-600 text-xl">
           参加メンバー5人
         </div>
         <div className="flex">
-          <div className=" bg-gray-600 rounded-full h-20 w-20 flex justify-center"></div>
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={36}
+            height={36}
+            className="my-6 rounded-full"
+          ></Image>
           <div className="font-bold my-auto mx-4 text-gray-800">
             ユーザー名ユーザー名ユーザー名ユーザー名
           </div>
         </div>
-        <div className="flex mt-4">
-          <div className=" bg-gray-600 rounded-full h-20 w-20 flex justify-center"></div>
+        <div className="flex mt-2">
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={36}
+            height={36}
+            className="my-6 rounded-full"
+          ></Image>
           <div className="font-bold my-auto mx-4 text-gray-800">
             ユーザー名ユーザー名ユーザー名ユーザー名
           </div>
         </div>
-        <div className="flex mt-4">
-          <div className=" bg-gray-600 rounded-full h-20 w-20 flex justify-center"></div>
+        <div className="flex mt-1">
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={36}
+            height={36}
+            className="my-6 rounded-full"
+          ></Image>
           <div className="font-bold my-auto mx-4 text-gray-800">
             ユーザー名ユーザー名ユーザー名ユーザー名
           </div>
         </div>
-        <div className="flex mt-4">
-          <div className=" bg-gray-600 rounded-full h-20 w-20 flex justify-center"></div>
+        <div className="flex mt-1">
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={36}
+            height={36}
+            className="my-6 rounded-full"
+          ></Image>
           <div className="font-bold my-auto mx-4 text-gray-800">
             ユーザー名ユーザー名ユーザー名ユーザー名
           </div>
         </div>
-        <div className="flex mt-4">
-          <div className=" bg-gray-600 rounded-full h-20 w-20 flex justify-center"></div>
+        <div className="flex mt-1">
+          <Image
+            src={"https://placehold.jp/150x150.png"}
+            alt={"shop_icon_url"}
+            width={36}
+            height={36}
+            className="my-6 rounded-full"
+          ></Image>
           <div className="font-bold my-auto mx-4 text-gray-800">
             ユーザー名ユーザー名ユーザー名ユーザー名
           </div>
         </div>
       </div>
-      <footer className="mt-14 mb-10 flex justify-between">
-        <button className="text-white bg-gray-800 px-8 py-5 font-bold mx-auto rounded-lg self-center hover:bg-gray-700 ">
+      <footer className="mt-14 mb-10 flex ">
+        <button className="text-white bg-gray-800 px-8 py-5 font-bold  rounded-lg self-center hover:bg-gray-700 mx-6">
           参加しない
         </button>
-        <button className="text-white bg-orange-500 px-12 py-5 font-bold mx-auto rounded-lg self-center hover:bg-orange-400 ">
+        <button className="text-white bg-orange-500 px-16 py-5 font-bold  rounded-lg self-center hover:bg-orange-400 mx-6">
           新しいプロフィールで参加
         </button>
       </footer>
