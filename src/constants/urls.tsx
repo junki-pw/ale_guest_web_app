8930553e1596d2972486948edea6c1f0b5a7822a
junki-pw <juken.master1234@gmail.com>
2023-10-23 13:59:46 +0900

    initialIcon 追加

---
+++ src/constants/urls.tsx
@@ -0,0 +1,3 @@
+/// デフォルトのアイコン
+export const initialIcon =
+  "https://firebasestorage.googleapis.com/v0/b/galapo-68a5a.appspot.com/o/other%2FinitialIcon.jpeg?alt=media&token=1fb0f093-2514-4ec2-812e-4a961aa42fd8";
