ada7e43586db3e24c6159a88dccb60b3dd04b251
junki-pw <juken.master1234@gmail.com>
2023-10-23 13:54:37 +0900

    RecoilProvider , InitialAuthProvider の追加

---
+++ src/providers/initial.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import useSWR from "swr";
+import { auth } from "./firebase";
+import { insertUserInfo } from "@/api/insertUserInfo";
+
+const initialFetcher = async () => {
+  await auth.authStateReady();
+  const userId = auth.currentUser?.uid;
+  if (userId == undefined) {
+    return;
+  }
+
+  // user データを取得して、データが無い場合は新規作成
+  await insertUserInfo();
+};
+
+const InitialAuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { isLoading, error } = useSWR("initial", initialFetcher);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center pt-40">
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else if (error) {
+    return <div>{error}</div>;
+  }
+
+  return <div>{children}</div>;
+};
+
+export default InitialAuthProvider;
