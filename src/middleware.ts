2bb02b556f7528c61cb83982ce057b547ca4ce63
junki-pw <juken.master1234@gmail.com>
2023-10-23 13:59:37 +0900

    middleware 更新

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,29 @@
-import { auth } from "@/providers/firebase";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-let _initialRedirect: boolean = true;
-let _initialPath: string | null = null;
+let initialPath: string | null = null;
 let isInitialUrlGone: boolean = false;
 
 const notLoginUserAvailablePagePaths = ["/login", "/"];
 
 export async function middleware(req: NextRequest) {
   const pathName = req.nextUrl.pathname;
-  console.log("URL: " + pathName);
+  console.log("RedirectURL: " + pathName);
 
-  const firebaseUser = auth.currentUser;
-  console.log("firebaseUser: " + firebaseUser?.uid);
+  if (initialPath != null) {
+    return;
+  }
 
   // 初期化処理
-  if (_initialRedirect) {
-    _initialRedirect = false;
-    _initialPath = pathName;
-    console.log("initialPath: " + pathName);
+  initialPath = pathName;
+  console.log("initialPath: " + pathName);
+
+  if (notLoginUserAvailablePagePaths.indexOf(pathName) != -1) {
+    return;
   }
 
-  if (firebaseUser == undefined) {
-    console.log("未認証ページに遷移");
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log("firebaseUser2: " + auth.currentUser?.uid);
-
-    if (notLoginUserAvailablePagePaths.indexOf(pathName) != -1) {
-      return;
-    }
-
-    // register, login, invite-email, pass-reset, home 以外は login に飛ばす
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  // home に飛ばして初期化処理すれば良さそう（初期化処理の後に画面遷移する）
-
-  //todo 認証していない場合は認証ページに飛ばす
+  // 認証していない場合は認証してないユーザーでも閲覧可能なページに飛ばす
+  return NextResponse.redirect(new URL("/login", req.url));
 }
 
 export const config = {
