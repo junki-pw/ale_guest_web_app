17810b57d03a3d535cd18be3d4d0b8ab35d59812
junki-pw <juken.master1234@gmail.com>
2023-10-25 07:14:09 +0900

    menuNoImageUrl 追加

--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,3 +1,7 @@
 /// デフォルトのアイコン
 export const initialIcon =
   "https://firebasestorage.googleapis.com/v0/b/galapo-68a5a.appspot.com/o/other%2FinitialIcon.jpeg?alt=media&token=1fb0f093-2514-4ec2-812e-4a961aa42fd8";
+
+/// メニューアイコン
+export const menuNoImageUrl =
+  "https://firebasestorage.googleapis.com/v0/b/quuuuuuuuu-930f6.appspot.com/o/admin_icons%2Fno-images%2Fno-image-camera.png?alt=media&token=a2dd6e6f-8144-4eb7-81f0-ca163e8ccf00";
