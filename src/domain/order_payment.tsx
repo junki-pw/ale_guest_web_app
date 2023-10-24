7b759de7257d68ef46009304001fa50dea08a843
junki-pw <juken.master1234@gmail.com>
2023-10-24 15:38:37 +0900

    domain 更新

--- src/domain/order_payment.tsx
+++ src/domain/order_payment.tsx
@@ -1,5 +1,48 @@
+import { DocumentData, FieldValue } from "firebase/firestore";
+
 export interface OrderPayment {
   orderPaymentId: string;
-  status: string;
+  shopId: string;
+  userId: string;
+  orderRoomId: string;
   amount: number;
+  appetizerAmount: number;
+  coverChargeAmount: number;
+  orderAmount: number;
+  status: string;
+  changeAmount: number | null;
+  cashAmount: number | null;
+  createdAt: Date | FieldValue | null;
+  updatedAt: Date | FieldValue | null;
 }
+
+export const orderPaymentFromJson = (data: DocumentData): OrderPayment => {
+  return {
+    orderPaymentId: data.orderPaymentId,
+    shopId: data.shopId,
+    userId: data.userId,
+    orderRoomId: data.orderRoomId,
+    amount: data.amount,
+    appetizerAmount: data.appetizerAmount,
+    coverChargeAmount: data.coverChargeAmount,
+    orderAmount: data.orderAmount,
+    status:
+      data.status == undefined || data.status == null ? "request" : data.status,
+    changeAmount:
+      data.changeAmount == undefined || data.changeAmount == null
+        ? null
+        : data.changeAmount,
+    cashAmount:
+      data.cashAmount == undefined || data.cashAmount == null
+        ? null
+        : data.cashAmount,
+    createdAt:
+      data.createdAt == undefined || data.createdAt == null
+        ? null
+        : data.createdAt.toDate(),
+    updatedAt:
+      data.updatedAt == undefined || data.updatedAt == null
+        ? null
+        : data.updatedAt.toDate(),
+  };
+};
--- src/domain/payer.tsx
+++ src/domain/payer.tsx
@@ -1,5 +1,54 @@
+import { DocumentData, FieldValue } from "firebase/firestore";
+
 export interface Payer {
   payerId: string;
-  status: string;
+  payerIcon: string;
+  payerName: string;
   amount: number;
+  status: string;
+  memo: string;
+  changerId: string | null;
+  cancelPaymentTransactionId: string | null;
+  orderPaymentTransactionId: string | null;
+  receiptUrl: string | null;
+  createdAt: Date | FieldValue | null;
+  updatedAt: Date | FieldValue | null;
 }
+
+export const payerFromJson = (data: DocumentData): Payer => {
+  return {
+    payerId: data.payerId,
+    payerIcon: data.payerIcon,
+    payerName: data.payerName,
+    amount: data.amount,
+    status:
+      data.status == undefined || data.status == null ? "request" : data.status,
+    memo: data.memo == undefined || data.memo == null ? "" : data.memo,
+    changerId:
+      data.changerId == undefined || data.changerId == null
+        ? null
+        : data.changerId,
+    cancelPaymentTransactionId:
+      data.cancelPaymentTransactionId == undefined ||
+      data.cancelPaymentTransactionId == null
+        ? null
+        : data.cancelPaymentTransactionId,
+    orderPaymentTransactionId:
+      data.orderPaymentTransactionId == undefined ||
+      data.orderPaymentTransactionId == null
+        ? null
+        : data.orderPaymentTransactionId,
+    receiptUrl:
+      data.receiptUrl == undefined || data.receiptUrl == null
+        ? null
+        : data.receiptUrl,
+    createdAt:
+      data.createdAt == undefined || data.createdAt == null
+        ? null
+        : data.createdAt.toDate(),
+    updatedAt:
+      data.updatedAt == undefined || data.updatedAt == null
+        ? null
+        : data.updatedAt.toDate(),
+  };
+};
