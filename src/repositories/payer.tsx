de9a739029a34cd4111fcc7ec22a972f4d0aa9c6
junki-pw <juken.master1234@gmail.com>
2023-10-25 17:06:14 +0900

    getPayers 追加

--- src/repositories/payer.tsx
+++ src/repositories/payer.tsx
@@ -1,7 +1,7 @@
 import { doc_not_found } from "@/constants/error";
 import { Payer, payerFromJson } from "@/domain/payer";
 import { db } from "@/providers/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query } from "firebase/firestore";
 
 export const getPayerById: (
   orderPaymentId: string,
@@ -20,4 +20,13 @@ export const getPayerById: (
     }
     return payerFromJson(value.data()!);
   });
+};
+
+export const getPayers: (orderPaymentId: string) => Promise<Payer[]> = async (
+  orderPaymentId: string
+) => {
+  const q = query(collection(db, "order_payments", orderPaymentId, "payers"));
+  return getDocs(q).then((value) =>
+    value.docs.map((e) => payerFromJson(e.data()))
+  );
 };
