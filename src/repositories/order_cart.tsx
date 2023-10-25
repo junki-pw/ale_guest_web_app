1b4d56295fb81f20960a264785f49bd047689f70
junki-pw <juken.master1234@gmail.com>
2023-10-25 17:06:23 +0900

    getOrderCarts 追加

--- src/repositories/order_cart.tsx
+++ src/repositories/order_cart.tsx
@@ -1,9 +1,10 @@
-import { orderCartFromJson } from "@/domain/order_cart";
+import { OrderCart, orderCartFromJson } from "@/domain/order_cart";
 import { db } from "@/providers/firebase";
 import {
   Unsubscribe,
   collection,
   getCountFromServer,
+  getDocs,
   onSnapshot,
   orderBy,
   query,
@@ -32,4 +33,16 @@ export const getOrderedCount: (orderRoomId: string) => Promise<number> = async (
   );
 
   return await getCountFromServer(q).then((value) => value.data().count);
+};
+
+export const getOrderCarts: (
+  orderRoomId: string
+) => Promise<OrderCart[]> = async (orderRoomId: string) => {
+  const q = query(
+    collection(db, "order_carts"),
+    where("orderRoomId", "==", orderRoomId)
+  );
+  return await getDocs(q).then((value) =>
+    value.docs.map((e) => orderCartFromJson(e.data()))
+  );
 };
