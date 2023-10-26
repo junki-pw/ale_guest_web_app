8e164a670bad5c3fb3f5fe7ea5f6da5b5a040867
junki-pw <juken.master1234@gmail.com>
2023-10-26 15:35:08 +0900

    status constants 追加

--- src/constants/keys.tsx
+++ src/constants/keys.tsx
@@ -9,3 +9,9 @@ export const isDeleted = "isDeleted";
 export const isActive = "isActive";
 export const isHidden = "isHidden";
 export const isClosed = "isClosed";
+
+// status
+export const kRequest = "request";
+export const kCash = "cash";
+export const kCard = "card";
+export const kCancel = "cancel";
