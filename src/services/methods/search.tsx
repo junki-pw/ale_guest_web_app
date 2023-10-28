bc1e1cf00f5009ecdfc8039d7611ee2b9f2b6a89
junki-pw <juken.master1234@gmail.com>
2023-10-28 15:36:19 +0900

    戻り値の型を設定

--- src/services/methods/search.tsx
+++ src/services/methods/search.tsx
@@ -1,4 +1,5 @@
 import {
+  errorCategory,
   errorMenu,
   errorOption,
   errorOrderCart,
@@ -10,7 +11,10 @@ import { ShopMenu } from "@/domain/shop_menu";
 import { MenuOption } from "@/domain/shop_option";
 import { ShopSeat } from "@/domain/shop_seat";
 
-export const searchMenu = (menus: ShopMenu[], menuId: string | null) => {
+export const searchMenu: (
+  menus: ShopMenu[],
+  menuId: string | null
+) => ShopMenu = (menus: ShopMenu[], menuId: string | null) => {
   const index = menus.findIndex((element) => element.menuId == menuId);
   if (index == -1) {
     return errorMenu;
@@ -18,10 +22,10 @@ export const searchMenu = (menus: ShopMenu[], menuId: string | null) => {
   return menus[index];
 };
 
-export const searchOption = (
+export const searchOption: (
   options: MenuOption[],
   optionId: string | null
-) => {
+) => MenuOption = (options: MenuOption[], optionId: string | null) => {
   const index = options.findIndex((element) => element.optionId == optionId);
   if (index == -1) {
     return errorOption;
@@ -29,23 +33,23 @@ export const searchOption = (
   return options[index];
 };
 
-export const searchCategory = (
+export const searchCategory: (
   categories: MenuCategory[],
   categoryId: string | null
-) => {
+) => MenuCategory = (categories: MenuCategory[], categoryId: string | null) => {
   const index = categories.findIndex(
     (element) => element.categoryId == categoryId
   );
   if (index == -1) {
-    return errorOption;
+    return errorCategory;
   }
   return categories[index];
 };
 
-export const searchOrderCart = (
+export const searchOrderCart: (
   orderCarts: OrderCart[],
   orderCartId: string | null
-) => {
+) => OrderCart = (orderCarts: OrderCart[], orderCartId: string | null) => {
   const index = orderCarts.findIndex(
     (element) => element.orderCartId == orderCartId
   );
@@ -55,7 +59,10 @@ export const searchOrderCart = (
   return orderCarts[index];
 };
 
-export const searchSeat = (seats: ShopSeat[], seatId: string | null) => {
+export const searchSeat: (
+  seats: ShopSeat[],
+  seatId: string | null
+) => ShopSeat = (seats: ShopSeat[], seatId: string | null) => {
   const index = seats.findIndex((element) => element.seatId == seatId);
   if (index == -1) {
     return errorSeat;
