1e791e6ffed8849a29fb9545d2973f31d3875404
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-10-28 11:16:59 +0900

    stateとfetcher

--- src/app/order-rooms/[orderRoomId]/check-join/fetcher.tsx
+++ src/app/order-rooms/[orderRoomId]/check-join/fetcher.tsx
@@ -4,6 +4,12 @@ import { getOrderRoomById } from "@/repositories/order_room";
 import { CheckJoinState } from "./state";
 import { Shop } from "@/domain/shop";
 import { getShopById } from "@/repositories/shop";
+import { getSeatById } from "@/repositories/shop_seat";
+import { CoverCharge } from "@/domain/cover_charge";
+import { getCoverCharge } from "@/repositories/cover_charge";
+import { getOrderRoomUsers } from "@/repositories/order_room_user";
+import { OrderRoomUser } from "@/domain/order_room_user";
+import { getOrderedCount } from "@/repositories/order_cart";
 
 export const checkJoinFetcher: (
   orderRoomId: string
@@ -11,12 +17,24 @@ export const checkJoinFetcher: (
   //orderRoomを取得
   const orderRoom: OrderRoom = await getOrderRoomById(orderRoomId);
   //シート取得
-  //   const seat:ShopSeat = await getSeatById(orderRoom.seatId);
+  const seat: ShopSeat = await getSeatById(orderRoom.seatId);
   //coverCharge
-
+  const coverCharge: CoverCharge = await getCoverCharge(orderRoom.shopId);
   //shop
   const shop: Shop = await getShopById(orderRoom.shopId);
   //orderRoomUsers
+  const orderRoomUsers: OrderRoomUser[] = await getOrderRoomUsers(orderRoomId);
+
   //オーダー数
-  return { orderRoom: orderRoom, shop: shop };
+  const orderCount: number = await getOrderedCount(orderRoomId);
+
+  return {
+    orderRoom: orderRoom,
+    seat: seat,
+    coverCharge: coverCharge,
+    shop: shop,
+    orderRoomUsers: orderRoomUsers,
+    orderedCount: orderCount,
+    isLoading: false,
+  };
 };
--- src/app/order-rooms/[orderRoomId]/check-join/state.tsx
+++ src/app/order-rooms/[orderRoomId]/check-join/state.tsx
@@ -7,9 +7,9 @@ import { ShopSeat } from "@/domain/shop_seat";
 export interface CheckJoinState {
   orderRoom: OrderRoom;
   shop: Shop;
-  //   seat: ShopSeat;
-  //   coverCharge: CoverCharge | null;
-  //   orderRoomUsers: OrderRoomUser[];
-  //   orderedCount: number;
-  //   isLoading: boolean;
+    seat: ShopSeat;
+    coverCharge: CoverCharge | null;
+    orderRoomUsers: OrderRoomUser[];
+    orderedCount: number;
+    isLoading: boolean;
 }
