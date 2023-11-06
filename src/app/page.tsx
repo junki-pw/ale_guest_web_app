e6c0f2cdce2be0c7c28d80343dbf6a4c38a31fdf
junki-pw <juken.master1234@gmail.com>
2023-11-06 16:18:18 +0900

    home page の修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { getOrderCartsContainedUnLimitedPlanById } from "@/repositories/order_cart";
-
 export default function Home() {
-  async function handleOnClicked() {
-    const orderRoomId = "919a2da0-2506-42bb-86ef-713033374523";
-    getOrderCartsContainedUnLimitedPlanById(orderRoomId);
-  }
-
   return (
-    <main className="flex flex-col pb-[120px]">
-      <button onClick={handleOnClicked}>現在の時刻を取得する</button>
+    <main>
+      <button>Home Page</button>
     </main>
   );
 }
