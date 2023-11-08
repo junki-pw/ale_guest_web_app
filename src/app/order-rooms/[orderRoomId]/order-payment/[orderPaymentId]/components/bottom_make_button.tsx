7290ca4667796229bdf7902187f7e320a35f4391
junki-pw <juken.master1234@gmail.com>
2023-11-08 16:48:47 +0900

    一部のUIを修正

--- src/app/order-rooms/[orderRoomId]/order-payment/[orderPaymentId]/components/bottom_make_button.tsx
+++ src/app/order-rooms/[orderRoomId]/order-payment/[orderPaymentId]/components/bottom_make_button.tsx
@@ -1,27 +1,35 @@
 import { makePayment } from "@/repositories/order_payment";
+import { useRouter } from "next/navigation";
 
 interface Props {
   orderPaymentId: string;
 }
 
 export default function BottomMakeButton({ orderPaymentId }: Props) {
+  const router = useRouter();
+
   // お支払いを確定
   async function handleMakePayment() {
     const alertMessage: string =
       "お支払いを確定しても宜しいですか？\n後から変更したい場合はキャンセルできます。";
     if (window.confirm(alertMessage)) {
-      await makePayment(orderPaymentId).catch((e) => {
-        alert(e);
-      });
+      await makePayment(orderPaymentId)
+        .then((value) => {
+          alert("お会計を送信しました");
+          router.back();
+        })
+        .catch((e) => alert(e));
     }
   }
 
   return (
-    <button
-      className="fixed bottom-3 px-4 w-full py-3 bg-orange-400 rounded-md font-bold text-white"
-      onClick={handleMakePayment}
-    >
-      お支払いを確定する
-    </button>
+    <div className="fixed bottom-0 w-full px-4 py-3">
+      <button
+        className="bg-orange-400 rounded-md font-bold text-white w-full py-3"
+        onClick={handleMakePayment}
+      >
+        お支払いを確定する
+      </button>
+    </div>
   );
 }
