bf5d6e0d870dd05500335832da168dea8068c901
junki-pw <juken.master1234@gmail.com>
2023-11-09 06:42:21 +0900

    unSub を考慮した処理に変更

--- src/app/order-rooms/[orderRoomId]/order-carts/hooks.tsx
+++ src/app/order-rooms/[orderRoomId]/order-carts/hooks.tsx
@@ -1,6 +1,9 @@
 import useSWRSubscription from "swr/subscription";
-import { streamOrderCartsByOrderRoomId } from "@/repositories/order_cart";
+import { getLocalOrderCarts } from "@/repositories/order_cart";
 import useSWR from "swr";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "@/providers/firebase";
+import { orderCartsCollection } from "@/constants/firebase";
 
 export function useStreamOrderCartsById(orderRoomId: string) {
   const { data, mutate } = useSWR(`order-rooms/${orderRoomId}/order-carts`);
@@ -8,9 +11,18 @@ export function useStreamOrderCartsById(orderRoomId: string) {
   useSWRSubscription(
     `/useStreamOrderCartsById/${orderRoomId}`,
     (key, { next }) => {
-      streamOrderCartsByOrderRoomId(data.orderRoom.orderRoomId, (orderCarts) =>
-        mutate({ ...data, orderCarts }, false)
+      const q = query(
+        collection(db, orderCartsCollection),
+        where("orderRoomId", "==", orderRoomId)
       );
+
+      const unSub = onSnapshot(q, () =>
+        getLocalOrderCarts(data.orderRoom.orderRoomId).then((orderCarts) =>
+          mutate({ ...data, orderCarts }, false)
+        )
+      );
+
+      return () => unSub && unSub();
     }
   );
 }
