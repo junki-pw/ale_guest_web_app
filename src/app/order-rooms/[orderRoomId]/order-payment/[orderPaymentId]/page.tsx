ad3d4eccaa8f30b81c238a1aca899a989f01543c
junki-pw <juken.master1234@gmail.com>
2023-11-08 16:49:02 +0900

    微細な更新

--- src/app/order-rooms/[orderRoomId]/order-payment/[orderPaymentId]/page.tsx
+++ src/app/order-rooms/[orderRoomId]/order-payment/[orderPaymentId]/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { Payer } from "@/domain/payer";
-import { auth, db } from "@/providers/firebase";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
 import BottomMakeButton from "./components/bottom_make_button";
 import useSWR from "swr";
 import { orderPaymentFetcher } from "./fetcher";
@@ -29,30 +25,32 @@ export default function OrderPaymentPage({
   }
 
   return (
-    <main className="relative h-full py-6 px-4">
-      {/* // 合計金額 */}
-      <h1 className="text-lg font-bold ">合計金額</h1>
-      <h2 className="my-1.5 text-3xl text-orange-500 font-bold">
-        ¥ {data!.payer?.amount}
-      </h2>
-      <div className="h-[1px] w-full bg-gray-300"></div>
+    <main className="relative w-full h-full py-6">
+      <div className="px-4">
+        {/* // 合計金額 */}
+        <h1 className="text-lg font-bold ">合計金額</h1>
+        <h2 className="my-1.5 text-3xl text-orange-500 font-bold">
+          ¥ {data!.payer?.amount.toLocaleString()}
+        </h2>
+        <div className="h-[1px] w-full bg-gray-300"></div>
 
-      {/* // お支払い方法 */}
-      <h1 className="text-lg font-bold mt-8 mb-3">お支払い方法</h1>
-      <div className="flex">
-        <_Button isCardPayment={true} />
-        <_Button isCardPayment={false} />
+        {/* // お支払い方法 */}
+        <h1 className="text-lg font-bold mt-8 mb-3">お支払い方法</h1>
+        <div className="flex">
+          <_Button isCardPayment={true} />
+          <_Button isCardPayment={false} />
+        </div>
+        <p className="text-xs text-gray-400 mt-4">
+          大変申し訳ありませんがWEB版ではカード支払いが出来ません。
+          <br />
+          次回以降のアップデートで出来るように努めます。
+          <br />
+          <br />
+          また、アプリ上でのカード決済ではあなたがお会計をすると
+          <br />
+          スタッフの詳細を見れたり評価をしたり出来ます。
+        </p>
       </div>
-      <p className="text-xs text-gray-400 mt-4">
-        大変申し訳ありませんがWEB版ではカード支払いが出来ません。
-        <br />
-        次回以降のアップデートで出来るように努めます。
-        <br />
-        <br />
-        また、アプリ上でのカード決済ではあなたがお会計をすると
-        <br />
-        スタッフの詳細を見れたり評価をしたり出来ます。
-      </p>
 
       <BottomMakeButton orderPaymentId={orderPaymentId} />
     </main>
